b044fee6c6a37aa48976f7ff55eadad1e528dcea
pinchao <cpc4253@163.com>
2023-02-25 22:58:54 +0800

    尝试一个测试demo

--- cypress/e2e/3-first_test/spec3.cy.js
+++ cypress/e2e/3-first_test/spec3.cy.js
@@ -1,3 +1,9 @@
-describe("", () => {
-  it("Do not do much!", () => {});
+describe("访问 Bing", () => {
+  it("能够搜索", () => {
+    cy.visit("https://bing.com");
+    cy.get("#sb_form_q").type("柯南");
+    cy.get("#search_icon").click();
+    cy.contains("名侦探柯南").should("exist");
+    cy.contains("普通话版").should("exist");
+  });
 });
